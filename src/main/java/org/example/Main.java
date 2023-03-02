a8f4ffb07f237b8565a1e46f7921290b50dba61a
kvog82 <kvog82@gmail.com>
2023-03-02 08:49:10 +0100

    added good vs evil in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,5 @@
 package org.example;
 
-import org.example.directionsReducer.DirReduction;
-import org.example.numbersExpandedForm.NumberExpander;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my Codewars practice project!");
