5af9db62c73a56179ca208cd670ccfa53572dd0e
kvog82 <kvog82@gmail.com>
2023-03-30 16:07:28 +0200

    added sort the odd

--- src/main/java/org/example/sortTheOdd/OddSorter.java
+++ src/main/java/org/example/sortTheOdd/OddSorter.java
@@ -1,2 +1,29 @@
-package org.example.sortTheOdd;public class oddSorter {
+package org.example.sortTheOdd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OddSorter {
+    public int[] sortArray(int[] array) {
+        List<Integer> sorted = new ArrayList<>();
+        List<Integer> odds = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                odds.add(array[i]);
+            }
+        }
+        Collections.sort(odds);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                sorted.add(odds.get(0));
+                odds.remove(0);
+            } else {
+                sorted.add(array[i]);
+            }
+        }
+
+        return sorted.stream().mapToInt(i -> i).toArray();
+    }
 }
--- src/test/java/org/example/sortTheOdd/OddSorterTest.java
+++ src/test/java/org/example/sortTheOdd/OddSorterTest.java
@@ -1,6 +1,32 @@
 package org.example.sortTheOdd;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+
 class OddSorterTest {
-  
+
+    OddSorter oddSorter = new OddSorter();
+
+    public static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{}, new int[]{}),
+                Arguments.of(new int[]{ 1, 7 }, new int[]{ 7, 1 }),
+                Arguments.of(new int[]{ 3, 8, 6, 5, 4 }, new int[]{ 5, 8, 6, 3, 4 }),
+                Arguments.of(new int[]{ 1, 3, 5, 8, 0 }, new int[]{ 5, 3, 1, 8, 0 }),
+                Arguments.of(new int[]{ 1, 8, 3, 6, 5, 4, 7, 2, 9, 0 }, new int[]{ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void getSortedArray(int[] expected, int[] inputArray) {
+        int[] result = oddSorter.sortArray(inputArray);
+
+        Assertions.assertArrayEquals(expected, result);
+    }
 }
