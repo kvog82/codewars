598b665fde7ae2f1befedef669465e1e6a445c31
kvog82 <kvog82@gmail.com>
2023-03-30 16:06:01 +0200

    added sort the odd

---
+++ src/main/java/org/example/sortTheOdd/OddSorter.java
@@ -0,0 +1,2 @@
+package org.example.sortTheOdd;public class oddSorter {
+}
---
+++ src/test/java/org/example/sortTheOdd/OddSorterTest.java
@@ -0,0 +1,6 @@
+package org.example.sortTheOdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OddSorterTest {
+  
+}
