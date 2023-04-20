56e04fd25354b473a89fadb74a25f7c91ed649c2
kvog82 <kvog82@gmail.com>
2023-04-20 19:12:47 +0200

    refactored reverseWords

--- php/reverseWords/WordReverser.php
+++ php/reverseWords/WordReverser.php
@@ -4,7 +4,8 @@ namespace Test\reverseWords;
 
 class WordReverser
 {
-    function reverseWords($str) {
+    function reverseWords($str)
+    {
         $wordArray = explode(" ", $str);
         $reversedWordsArray = $this->getReversedWords($wordArray);
         return implode(" ", $reversedWordsArray);
