bc4055b3fdcda1510770fce7c32d9d3335385658
kvog82 <kvog82@gmail.com>
2023-05-27 10:19:09 +0200

    added human readable time in java

---
+++ java/src/main/java/org/example/HumanReadableTime/TimeConverter.java
@@ -0,0 +1,31 @@
+package org.example.HumanReadableTime;
+
+// Human Readable Time: https://www.codewars.com/kata/52685f7382004e774f0001f7
+
+public class TimeConverter {
+    public String makeReadable(int seconds) {
+        StringBuilder readableTime = new StringBuilder();
+        int minutes = 0;
+        int hours = 0;
+        if(seconds > 59) {
+            minutes = seconds/60;
+            seconds = seconds%60;
+            if(minutes > 59) {
+                hours = minutes/60;
+                minutes = minutes%60;
+            }
+        }
+        readableTime.append(getFormattedTimePart(hours) + ":");
+        readableTime.append(getFormattedTimePart(minutes) + ":");
+        readableTime.append(getFormattedTimePart(seconds));
+
+        return String.valueOf(readableTime);
+    }
+
+    private String getFormattedTimePart(int timePart) {
+        if (timePart < 10) {
+            return "0" + timePart;
+        }
+        return String.valueOf(timePart);
+    }
+}
---
+++ java/src/test/java/org/example/HumanReadableTime/TimeConverterTest.java
@@ -0,0 +1,32 @@
+package org.example.HumanReadableTime;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeConverterTest {
+
+    TimeConverter timeConverter = new TimeConverter();
+
+    public static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("00:00:00", 0),
+                Arguments.of("00:00:05", 5),
+                Arguments.of("00:01:00", 60),
+                Arguments.of("23:59:59", 86399),
+                Arguments.of("99:59:59", 359999)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void timeConverterTest(String expected, int seconds) {
+        String result = timeConverter.makeReadable(seconds);
+
+        assertEquals(expected, result);
+    }
+}
